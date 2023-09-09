19261baa8aa316b5929f9dbddb5da3bd3e006697
OrlanDroyd <orlandroyd@gmail.com>
2023-09-09 19:25:27 -0400

    feature/authentication_screen: design AuthenticationScreen

--- app/src/main/java/com/gmail/orlandroyd/diarynotes/navigation/NavGraph.kt
+++ app/src/main/java/com/gmail/orlandroyd/diarynotes/navigation/NavGraph.kt
@@ -1,5 +1,6 @@
 package com.gmail.orlandroyd.diarynotes.navigation
 
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
@@ -7,6 +8,7 @@ import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
+import com.gmail.orlandroyd.diarynotes.presentation.screens.auth.AuthenticationScreen
 import com.gmail.orlandroyd.diarynotes.util.Constants.WRITE_SCREEN_ARGUMENT_ID
 
 @Composable
@@ -24,9 +26,13 @@ fun SetupNavGraph(
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 fun NavGraphBuilder.authenticationRoute() {
     composable(route = Screen.Authentication.route) {
-
+        AuthenticationScreen(
+            loadingState = false,
+            onButtonClicked = {}
+        )
     }
 }
 
---
+++ app/src/main/java/com/gmail/orlandroyd/diarynotes/presentation/screens/auth/AuthenticationScreen.kt
@@ -0,0 +1,32 @@
+package com.gmail.orlandroyd.diarynotes.presentation.screens.auth
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@ExperimentalMaterial3Api
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun AuthenticationScreen(
+    loadingState: Boolean,
+    onButtonClicked: () -> Unit,
+) {
+    Scaffold(
+        modifier = Modifier
+            .background(MaterialTheme.colorScheme.surface)
+            .statusBarsPadding()
+            .navigationBarsPadding(),
+        content = {
+            AuthenticationContent(
+                loadingState = loadingState,
+                onButtonClicked = onButtonClicked
+            )
+        }
+    )
+}
