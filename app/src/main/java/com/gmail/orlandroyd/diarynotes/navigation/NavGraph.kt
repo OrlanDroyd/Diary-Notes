57cf486e2fb0820fa5d73fa7f45bb4e068d6fa69
OrlanDroyd <orlandroyd@gmail.com>
2023-09-17 19:05:50 -0400

    feature/authentication_screen: add logout implementation

--- app/src/main/java/com/gmail/orlandroyd/diarynotes/navigation/NavGraph.kt
+++ app/src/main/java/com/gmail/orlandroyd/diarynotes/navigation/NavGraph.kt
@@ -5,7 +5,11 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
@@ -13,13 +17,19 @@ import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
+import com.gmail.orlandroyd.diarynotes.R
+import com.gmail.orlandroyd.diarynotes.presentation.components.DisplayAlertDialog
 import com.gmail.orlandroyd.diarynotes.presentation.screens.auth.AuthenticationScreen
 import com.gmail.orlandroyd.diarynotes.presentation.screens.auth.AuthenticationViewModel
 import com.gmail.orlandroyd.diarynotes.presentation.screens.home.HomeScreen
+import com.gmail.orlandroyd.diarynotes.util.Constants.APP_ID
 import com.gmail.orlandroyd.diarynotes.util.Constants.WRITE_SCREEN_ARGUMENT_ID
 import com.stevdzasan.messagebar.rememberMessageBarState
 import com.stevdzasan.onetap.rememberOneTapSignInState
+import io.realm.kotlin.mongodb.App
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 @Composable
 fun SetupNavGraph(
@@ -39,6 +49,10 @@ fun SetupNavGraph(
         homeRoute(
             navigateToWrite = {
                 navController.navigate(Screen.Write.route)
+            },
+            navigateToAuth = {
+                navController.popBackStack()
+                navController.navigate(Screen.Authentication.route)
             }
         )
         writeRoute()
@@ -106,10 +120,18 @@ fun NavGraphBuilder.authenticationRoute(navigateToHome: () -> Unit) {
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
-fun NavGraphBuilder.homeRoute(navigateToWrite: () -> Unit) {
+fun NavGraphBuilder.homeRoute(
+    navigateToWrite: () -> Unit,
+    navigateToAuth: () -> Unit,
+) {
     composable(route = Screen.Home.route) {
+
         val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
         val scope = rememberCoroutineScope()
+        var isSignOutDialogOpened by rememberSaveable {
+            mutableStateOf(false)
+        }
+
         HomeScreen(
             drawerState = drawerState,
             onMenuClicked = {
@@ -117,9 +139,27 @@ fun NavGraphBuilder.homeRoute(navigateToWrite: () -> Unit) {
                     drawerState.open()
                 }
             },
-            onSignOutClicked = {/*TODO*/ },
+            onSignOutClicked = { isSignOutDialogOpened = true },
             navigateToWrite = navigateToWrite,
         )
+
+        DisplayAlertDialog(
+            title = stringResource(R.string.sign_out),
+            message = stringResource(R.string.are_you_sure_you_want_to_sign_out_from_your_google_account),
+            dialogOpened = isSignOutDialogOpened,
+            onDialogClosed = { isSignOutDialogOpened = false },
+            onYesClicked = {
+                scope.launch(Dispatchers.IO) {
+                    val user = App.create(APP_ID).currentUser
+                    if (user != null) {
+                        user.logOut()
+                        withContext(Dispatchers.Main) {
+                            navigateToAuth()
+                        }
+                    }
+                }
+            })
+
     }
 }
 
