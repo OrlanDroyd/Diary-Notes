088eeb969081033536b41fd29edfb45372aff910
OrlanDroyd <orlandroyd@gmail.com>
2023-09-16 22:59:59 -0400

    feature/authentication_screen: add oneTap Sign in with Google

--- app/src/main/java/com/gmail/orlandroyd/diarynotes/navigation/NavGraph.kt
+++ app/src/main/java/com/gmail/orlandroyd/diarynotes/navigation/NavGraph.kt
@@ -2,6 +2,8 @@ package com.gmail.orlandroyd.diarynotes.navigation
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
@@ -9,7 +11,10 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
 import com.gmail.orlandroyd.diarynotes.presentation.screens.auth.AuthenticationScreen
+import com.gmail.orlandroyd.diarynotes.presentation.screens.auth.AuthenticationViewModel
 import com.gmail.orlandroyd.diarynotes.util.Constants.WRITE_SCREEN_ARGUMENT_ID
+import com.stevdzasan.messagebar.rememberMessageBarState
+import com.stevdzasan.onetap.rememberOneTapSignInState
 
 @Composable
 fun SetupNavGraph(
@@ -29,9 +34,58 @@ fun SetupNavGraph(
 @OptIn(ExperimentalMaterial3Api::class)
 fun NavGraphBuilder.authenticationRoute() {
     composable(route = Screen.Authentication.route) {
+
+        val viewModel: AuthenticationViewModel = viewModel()
+        val authenticated by viewModel.authenticated
+        val loadingState by viewModel.loadingState
+        val oneTapState = rememberOneTapSignInState()
+        val messageBarState = rememberMessageBarState()
+
         AuthenticationScreen(
-            loadingState = false,
-            onButtonClicked = {}
+//            authenticated = authenticated,
+            loadingState = loadingState,
+            oneTapState = oneTapState,
+            messageBarState = messageBarState,
+            onButtonClicked = {
+                oneTapState.open()
+                viewModel.setLoading(true)
+            },
+//            onSuccessfulFirebaseSignIn = { tokenId ->
+//                viewModel.signInWithMongoAtlas(
+//                    tokenId = tokenId,
+//                    onSuccess = {
+//                        messageBarState.addSuccess("Successfully Authenticated!")
+//                        viewModel.setLoading(false)
+//                    },
+//                    onError = {
+//                        messageBarState.addError(it)
+//                        viewModel.setLoading(false)
+//                    }
+//                )
+//            },
+//            onFailedFirebaseSignIn = {
+//                messageBarState.addError(it)
+//                viewModel.setLoading(false)
+//            },
+            onTokenIdReceived = { tokenId ->
+                viewModel.signInWithMongoAtlas(
+                    tokenId = tokenId,
+                    onSuccess = {
+                        if (it) {
+                            messageBarState.addSuccess("Successfully Authenticated!")
+                        }
+                        viewModel.setLoading(false)
+                    },
+                    onError = {
+                        messageBarState.addError(it)
+                        viewModel.setLoading(false)
+                    }
+                )
+            },
+            onDialogDismissed = { message ->
+                messageBarState.addError(Exception(message))
+                viewModel.setLoading(false)
+            },
         )
     }
 }
--- app/src/main/java/com/gmail/orlandroyd/diarynotes/presentation/screens/auth/AuthenticationScreen.kt
+++ app/src/main/java/com/gmail/orlandroyd/diarynotes/presentation/screens/auth/AuthenticationScreen.kt
@@ -9,13 +9,25 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.gmail.orlandroyd.diarynotes.util.Constants.CLIENT_ID
+import com.stevdzasan.messagebar.ContentWithMessageBar
+import com.stevdzasan.messagebar.MessageBarState
+import com.stevdzasan.onetap.OneTapSignInState
+import com.stevdzasan.onetap.OneTapSignInWithGoogle
 
 @ExperimentalMaterial3Api
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun AuthenticationScreen(
+    // authenticated: Boolean,
     loadingState: Boolean,
+    oneTapState: OneTapSignInState,
+    messageBarState: MessageBarState,
     onButtonClicked: () -> Unit,
+    onTokenIdReceived: (String) -> Unit,
+    onDialogDismissed: (String) -> Unit,
+//    onSuccessfulFirebaseSignIn: (String) -> Unit,
+//    onFailedFirebaseSignIn: (Exception) -> Unit,
 ) {
     Scaffold(
         modifier = Modifier
@@ -23,10 +35,32 @@ fun AuthenticationScreen(
             .statusBarsPadding()
             .navigationBarsPadding(),
         content = {
-            AuthenticationContent(
-                loadingState = loadingState,
-                onButtonClicked = onButtonClicked
-            )
+            ContentWithMessageBar(messageBarState = messageBarState) {
+                AuthenticationContent(
+                    loadingState = loadingState,
+                    onButtonClicked = onButtonClicked
+                )
+            }
+        }
+    )
+
+    OneTapSignInWithGoogle(
+        state = oneTapState,
+        clientId = CLIENT_ID,
+        onTokenIdReceived = { tokenId ->
+            onTokenIdReceived(tokenId)
+            /*val credential = GoogleAuthProvider.getCredential(tokenId, null)
+            FirebaseAuth.getInstance().signInWithCredential(credential)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        onSuccessfulFirebaseSignIn(tokenId)
+                    } else {
+                        task.exception?.let { it -> onFailedFirebaseSignIn(it) }
+                    }
+                }*/
+        },
+        onDialogDismissed = { message ->
+            onDialogDismissed(message)
         }
     )
 }
