aabd0b6bb0a587d5732bebef9df0fd84e2976942
OrlanDroyd <orlandroyd@gmail.com>
2023-09-18 23:27:57 -0400

    feature/home_screen: add DateHeader component

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,7 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+        isCoreLibraryDesugaringEnabled = true
     }
     kotlinOptions {
         jvmTarget = "17"
